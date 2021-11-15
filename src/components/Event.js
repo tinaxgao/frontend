886b0a74441c280b6a7b9c549317787429947421
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-11-15 14:44:29 -0500

    dummy text on event.js

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import axiosWithAuth from '../utils/axiosWithAuth';
+import React from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const Event = () => {
-
-    return (
-        <div className="event">
-            <h2>=======</h2>
-            <h2>Event.JS</h2>
+  return (
+    <div>
+      <section className="hero">
+        <h1>FirstName is hosting a Title at Location on Date</h1>
+        <p>
+          Description Description Description Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </section>
+      <section className="guest-list">
+        <button class="btn-primary">Attend</button>
+      </section>
+      <section className="guest-list">
+        <div className="dish-container">
+          <div className="guest">Guestname is bringing</div>
+          <div className="dish">Pumpkin Pie</div>
         </div>
-    )
-}
+        <div className="dish-container">
+          <div className="guest">Guestname is bringing</div>
+          <div className="dish">Apple Pie</div>
+        </div>
+        <div className="dish-container">
+          <div className="guest">Guestname is bringing</div>
+          <div className="dish">Pecan Pie</div>
+        </div>
+      </section>
+      <section className="event-edit">
+        <button class="btn-tertiary">Edit</button>
+        <button class="btn-tertiary">Delete</button>
+      </section>
+    </div>
+  );
+};
 
 export default Event;
