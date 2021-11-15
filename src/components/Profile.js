54738d072f0f08f10629d8afa2f61fbcbc891497
Sean Terry <seanterry@Seans-MBP.localdomain>
2021-11-15 12:44:07 -0800

    Updated Profile.js to include state, accepting props from login, making an axiosWithAuth call in a useEffect to load events, and Profile.css for minor formatting

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import EventList from './EventList';
-import CreateEvent from './CreateEvent';
+import { useNavigate } from 'react-router-dom';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const Profile = () => {
+import './Profile.css';
+
+
+const Profile = (props) => {
+    
+    const name = 'Sean'; // TEMP - Replace w. props.firstName here and in JSX
+    const [eventList, setEventList ] = useState([])
+    const navigate = useNavigate();
+
+    // ----- On click, navigate to Create Event form
+    const handleCreate = (e) => {
+        e.preventDefault();
+        navigate('/create-event');
+    }
+
+    // ----- Populate w. Potluck events the user created or is invited to
+    useEffect ( () => {
+        
+        /* add axios call
+        axiosWithAuth().get(`/events`)
+            .then( response => {
+                console.log('Profile.js: ', response)
+                setEventList(response.data)
+            })
+            .catch(error => {
+                console.log('Profile.js: ', error)
+            })
+        */
+       
+    }, []);
 
     return (
-        <div className="profile-container">
-            <h2>=======</h2>
+        <div className="container">
             <h2>Profile.JS</h2>
+            <div className="profile-header">
+                <h4>{name}'s Potluck</h4>
+                <button className="btn-secondary" onClick={handleCreate}>+ Create a Potluck</button>
+            </div>
+            <div className="event-list">
+                <EventList eventList={eventList}/>
+            </div>
         </div>
     )
 }
