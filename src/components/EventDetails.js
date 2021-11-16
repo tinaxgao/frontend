ea5492edfeba89e138bc268f0bfc93c6f829b50e
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-11-16 12:53:04 -0500

    swap event.js with eventdetails.js

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -1,14 +1,78 @@
-import React from 'react';
-import axiosWithAuth from '../utils/axiosWithAuth';
+import React, { useState, useEffect } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import GuestDetails from "./Guest-List/GuestDetails";
+
+import EventTitle from "./Guest-List/Event-Title/EventTitle";
+import EventTitleForm from "./Guest-List/Event-Title/EventTitleForm";
+import AddGuest from "./Guest-List/AddGuest";
+import useAttending from "../hooks/useAttending";
 
 const EventDetails = () => {
-
-    return (
-        <div className="event-details-container">
-            <h2>=======</h2>
-            <h2>EventDetails.JS</h2>
+    const initialState = {
+        firstName: "First Name",
+        title: "Potluck Title",
+        location: "Location",
+        date: "MM/DD/YY",
+        description: "Description Description Description",
+        guests: [
+          { id: 1, firstName: "Mario", dish: "Pumpkin Pie" },
+          { id: 2, firstName: "Luigi", dish: "Apple Pie" },
+          { id: 3, firstName: "Yoshi", dish: "Potatoes" },
+          { id: 4, firstName: "Peach", dish: "Sake" },
+        ],
+      };
+    
+      const [editing, setEditing] = useState(false);
+      
+      const handleToggle = () => {
+        setEditing(!editing);
+      };
+    
+      const [attending, handleAttending] = useAttending(false);
+    
+      useEffect(() => {
+        axiosWithAuth() //NOT THE CORRECT INFO YET, JUST TESTING API
+          .get()
+          .then((resp) => {
+            console.log(resp.data[0].first_name);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, []);
+    
+      
+    
+      return (
+        <div>
+          {!editing ? (
+            <EventTitle state={initialState} />
+          ) : (
+            <EventTitleForm state={initialState} />
+          )}
+          <section className="guest-list">
+            <button onClick={handleAttending} className="btn-primary">
+              Attend
+            </button>
+            <div className="attending">
+              {attending && (
+                <AddGuest attending={attending} handleAttending={handleAttending} />
+              )}
+            </div>
+          </section>
+          <section className="guest-list">
+            {initialState.guests.map((i) => (
+              <GuestDetails guests={i} key={i.id} />
+            ))}
+          </section>
+          <section className="event-edit">
+            <button onClick={handleToggle} class="btn-tertiary">
+              Edit
+            </button>
+            <button class="btn-tertiary">Delete</button>
+          </section>
         </div>
-    )
+      );
 }
 
 export default EventDetails;
--- src/components/Guest-List/AddGuest.js
+++ src/components/Guest-List/AddGuest.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 
 const AddGuest = (props) => {
-
   return (
     <div>
-      I'm attending and bringing ___
+      <form>
+        <input type="checkbox" id="isAttending" name="isAttending" />{" "}
+        <label>I'm attending.</label> <label> I will be bringing: </label>
+        <input
+          type="text"
+          id="bringingDish"
+          name="bringingDish"
+          placeholder="dish name"
+        />
+      </form>
       <button class="btn-primary">Submit</button>
       <button onClick={props.handleAttending} class="btn-tertiary">
         Cancel
