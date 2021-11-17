a9f9a29ead06d325a888e7aed5cc94425c4ab6d0
Michael Orvin <morvin1212@gmail.com>
2021-11-16 18:13:25 -0700

    Final update to signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './SignUp.css';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const submitCredentials = {
     firstName: "",
@@ -27,21 +27,13 @@ function SignUp () {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Submit Click: ", credentials);  // REMOVE WHEN AXIOS UPDATED
-        navigate('/login'); // REMOVE WHEN AXIOS UPDATED
-        /*
         axios.post('https://lambdapotluck.herokuapp.com/api/auth/register', credentials)
             .then( response => {
-                console.log('Sign-up: ', response);
-                localStorage.setItem('token', response.data.token);
                 navigate('/login');
-                setSubmitFailed(false);
             })
             .catch( error => {
                 console.log('Sign-up: ', error);
-                setSubmitFailed(true);
             })
-        */
     }
 
     
@@ -49,85 +41,71 @@ function SignUp () {
         <section className="registration-form">
             <form className="signUpForm" onSubmit={handleSubmit}>
                 <h2 className="title">Sign up for your account</h2>
-                <br/>
-                <h3 className="subTitle">to plan or attend a potluck!</h3>
-                <div className="name">
-                <div className="inputContainer">   
-                <label for="firstName" className="label">First Name</label>
-                <input 
-                    type="text" 
-                    className="input" 
-                    id="firstName" 
-                    name="firstName" 
-                    value={credentials.firstName} 
-                    onChange={handleChange}
-                    placeholder="a"
-                />
-                </div>
-                <div className="inputContainer">
-                <div className='last-name namefield'>       
-                <label for="lastName" className="label">Last Name</label>
-                </div>
-                <input 
-                    type="text" 
-                    className="input" 
-                    id="lastName" 
-                    name="lastName" 
-                    value={credentials.lastName} 
-                    onChange={handleChange}
-                    placeholder="a"
-                    
-                />
-                </div>
-                </div>
-                <div className="inputContainer">
-                    <div>       
-                    <label for="email" className="label">Email</label>
-                    </div>
-                    <input 
-                        type="email" 
-                        className="input" 
-                        id="email" 
-                        name="email"   
-                        value={credentials.email} 
-                        onChange={handleChange}
-                        placeholder="a"
-                    />
-                    <br/><br/>
+                    <div className="inputContainer">   
+                        <label for="firstName" className="label">First Name</label>
+                        <input 
+                            type="text" 
+                            className="input" 
+                            id="firstName" 
+                            name="firstName" 
+                            value={credentials.firstName} 
+                            onChange={handleChange}
+                            placeholder="a"
+                        />
                     </div>
                     <div className="inputContainer">
-                        <div>       
+                        <div className='last-name namefield'>       
+                            <label for="lastName" className="label">Last Name</label>
+                        </div>
+                        <input 
+                            type="text" 
+                            className="input" 
+                            id="lastName" 
+                            name="lastName" 
+                            value={credentials.lastName} 
+                            onChange={handleChange}
+                            placeholder="a"
+                        />
+                    </div>
+                    <div className="inputContainer">
+                        <label for="email" className="label">Email</label>
+                        <input 
+                            type="email" 
+                            className="input" 
+                            id="email" 
+                            name="email"   
+                            value={credentials.email} 
+                            onChange={handleChange}
+                            placeholder="a"
+                        />
+                    </div>
+                    <div className="inputContainer">
                     <label for="username" className="label">Username</label>
-                    </div>
-                    <input 
-                        type="text" 
-                        className="input" 
-                        id="username" 
-                        name="username"   
-                        value={credentials.username} 
-                        onChange={handleChange}
-                        placeholder="a" 
-                    />
-                    <br/><br/>
+                        <input 
+                            type="text" 
+                            className="input" 
+                            id="username" 
+                            name="username"   
+                            value={credentials.username} 
+                            onChange={handleChange}
+                            placeholder="a" 
+                        />
                     </div>
                     <div className="inputContainer">
-                        <div>       
                     <label for="password" className="label">Password</label>
-                    </div>
-                    <input 
-                        type="password" 
-                        className="input" 
-                        id="password" 
-                        name="password"   
-                        value={credentials.password} 
-                        onChange={handleChange}
-                        placeholder="a"
-                    />
-                    <br/><br/>
+                        <input 
+                            type="password" 
+                            className="input" 
+                            id="password" 
+                            name="password"   
+                            value={credentials.password} 
+                            onChange={handleChange}
+                            placeholder="a"
+                        />
                     </div>
                 <button type="submit" class="submitBtn" value="Sign up">Sign Up</button>
-                    <br/>
-                    {/* <p>Already have an account? Log in.</p> */}
+                <p>Already have an account?</p>
+                <Link to='/login'>Login</Link>
             </form>
         </section>   
     )
