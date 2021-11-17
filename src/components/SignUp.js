00518211a312dc280caf37b02ee5486d97264fba
Michael Orvin <morvin1212@gmail.com>
2021-11-16 17:27:48 -0700

    Finished Styling SignUp, and Cleaning up divs in SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -51,82 +51,69 @@ function SignUp () {
                 <h2 className="title">Sign up for your account</h2>
                 <br/>
                 <h3 className="subTitle">to plan or attend a potluck!</h3>
-                <div className="name">
-                <div className="inputContainer">   
-                <label for="firstName" className="label">First Name</label>
-                <input 
-                    type="text" 
-                    className="input" 
-                    id="firstName" 
-                    name="firstName" 
-                    value={credentials.firstName} 
-                    onChange={handleChange}
-                    placeholder="a"
-                />
-                </div>
-                <div className="inputContainer">
-                <div className='last-name namefield'>       
-                <label for="lastName" className="label">Last Name</label>
-                </div>
-                <input 
-                    type="text" 
-                    className="input" 
-                    id="lastName" 
-                    name="lastName" 
-                    value={credentials.lastName} 
-                    onChange={handleChange}
-                    placeholder="a"
-                    
-                />
-                </div>
-                </div>
-                <div className="inputContainer">
-                    <div>       
-                    <label for="email" className="label">Email</label>
-                    </div>
-                    <input 
-                        type="email" 
-                        className="input" 
-                        id="email" 
-                        name="email"   
-                        value={credentials.email} 
-                        onChange={handleChange}
-                        placeholder="a"
-                    />
-                    <br/><br/>
+                    <div className="inputContainer">   
+                        <label for="firstName" className="label">First Name</label>
+                        <input 
+                            type="text" 
+                            className="input" 
+                            id="firstName" 
+                            name="firstName" 
+                            value={credentials.firstName} 
+                            onChange={handleChange}
+                            placeholder="a"
+                        />
                     </div>
                     <div className="inputContainer">
-                        <div>       
+                        <div className='last-name namefield'>       
+                            <label for="lastName" className="label">Last Name</label>
+                        </div>
+                        <input 
+                            type="text" 
+                            className="input" 
+                            id="lastName" 
+                            name="lastName" 
+                            value={credentials.lastName} 
+                            onChange={handleChange}
+                            placeholder="a"
+                        />
+                    </div>
+                    <div className="inputContainer">
+                        <label for="email" className="label">Email</label>
+                        <input 
+                            type="email" 
+                            className="input" 
+                            id="email" 
+                            name="email"   
+                            value={credentials.email} 
+                            onChange={handleChange}
+                            placeholder="a"
+                        />
+                    </div>
+                    <div className="inputContainer">
                     <label for="username" className="label">Username</label>
-                    </div>
-                    <input 
-                        type="text" 
-                        className="input" 
-                        id="username" 
-                        name="username"   
-                        value={credentials.username} 
-                        onChange={handleChange}
-                        placeholder="a" 
-                    />
-                    <br/><br/>
+                        <input 
+                            type="text" 
+                            className="input" 
+                            id="username" 
+                            name="username"   
+                            value={credentials.username} 
+                            onChange={handleChange}
+                            placeholder="a" 
+                        />
                     </div>
                     <div className="inputContainer">
-                        <div>       
                     <label for="password" className="label">Password</label>
-                    </div>
-                    <input 
-                        type="password" 
-                        className="input" 
-                        id="password" 
-                        name="password"   
-                        value={credentials.password} 
-                        onChange={handleChange}
-                        placeholder="a"
-                    />
-                    <br/><br/>
+                        <input 
+                            type="password" 
+                            className="input" 
+                            id="password" 
+                            name="password"   
+                            value={credentials.password} 
+                            onChange={handleChange}
+                            placeholder="a"
+                        />
                     </div>
                 <button type="submit" class="submitBtn" value="Sign up">Sign Up</button>
-                    <br/>
                     {/* <p>Already have an account? Log in.</p> */}
             </form>
         </section>   
