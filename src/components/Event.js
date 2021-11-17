2ff7fbfa6aecc3640b3263d70c79bff523adcd68
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-11-17 12:30:00 -0700

    added code to eventlist and event

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,14 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import axiosWithAuth from "../utils/axiosWithAuth";
+import EventDetails from './EventDetails'
+import Profile from './Profile';
 
 
-const Event = () => {
- 
+  
 
+const Event = (props) => {
+  // const [event, setEvent] = useState({})
+  // const { organizer } = useParams()
+  const navigate = useNavigate();
+  const { organizer, date, title, location, event_id } = props.event
+  
+
+  
+  const handleClick =() => {
+    navigate(`/event-details/${organizer}`)
+  }
+  console.log(props.event)
   return (
-    <div>
-      Event that gets populated through eventlist on profile page
-    </div>
+  
+    <div className="event-list" key={event_id} onClick={handleClick}>
+        <div>
+        <p>Date:{date}</p>
+        <div className="dish-list">
+          {/* dish.map(dish => {<p>{dish}</p>}) */}
+        </div>
+        <h4>Name:{title}</h4> 
+        <p>Location:{location}</p>
+        <p>Host:{organizer}</p>
+        </div>
+  </div>
+
   );
 };
 
 export default Event;
+//map out list of dishes in dish list
+// dish.map(dish => {<p>{dish}</p>})
--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,52 +1,13 @@
 import React from 'react';
-// import Event from './Event';
-import axiosWithAuth from '../utils/axiosWithAuth';
-// import { useHistory } from 'react-router';
+import Event from './Event'
 
-const EventList = () => {
+
+const EventList = (props) => {
+    const { eventList } = props;
     
-    // const {push} = useHistory();
-    const handleSubmit = e => {
-        e.preventDefault();
-        // push("/create-event");
-    }
-
     return (
-        <div className="event-list-container">
-            <div className="event-list-heading">
-                <h2>FirstName's Potluck</h2>
-                <button onClick={handleSubmit} className="create-btn">+ Create a Potluck</button>
-            </div>
-            <div className="event-list">
-
-                
-                <p>MM/DD/YYYY</p>
-                <img src="https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"></img>
-                <h4>Event Title</h4>
-                <p>Location</p>
-                <p>hostname</p>
-            </div>
-            <div className="event-list">
-                <p>MM/DD/YYYY</p>
-                <img src="https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"></img>
-                <h4>Event Title</h4>
-                <p>Location</p>
-                <p>hostname</p>
-            </div>
-            <div className="event-list">
-                <p>MM/DD/YYYY</p>
-                <img src="https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"></img>
-                <h4>Event Title</h4>
-                <p>Location</p>
-                <p>hostname</p>
-            </div>
-            <div className="event-list">
-                <p>MM/DD/YYYY</p>
-                <img src="https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"></img>
-                <h4>Event Title</h4>
-                <p>Location</p>
-                <p>hostname</p>
-            </div>
+        <div className="event-list"> 
+            {eventList.map(event => <Event key={event.event_id} event={event}/>)}   
         </div>
     )
 }
