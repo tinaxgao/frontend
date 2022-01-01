e998aadb1142d154991f5bb143dfbbba3623686b
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2022-01-01 14:42:18 -0500

    useparams back to id

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router";
 
 import GuestDetails from "./Guest-List/GuestDetails";
 
@@ -10,77 +10,87 @@ import useAttending from "../hooks/useAttending";
 import axiosWithAuth from "../utils/axiosWithAuth";
 
 const EventDetails = (props) => {
-    const initialState = {
-        event_id: 123,
-        organizer: "host user id",
-        event_title: "Potluck Title",
-        event_location: "Location",
-        event_date: "MM/DD/YY",
-        event_description: "Description Description Description",
-        guests:
-          [ { id: 1, organizer: "Mario", bringingDish: "Pumpkin Pie" },
-          { id: 2, organizer: "Luigi", bringingDish: "Apple Pie" },
-          { id: 3, organizer: "Yoshi", bringingDish: "Potatoes" },
-          { id: 4, organizer: "Peach", bringingDish: "Sake" } ]
-      };
+  const initialState = {
+    event_id: 123,
+    organizer: "host user id",
+    event_title: "Potluck Title",
+    event_location: "Location",
+    event_date: "MM/DD/YY",
+    event_description: "Description Description Description",
+    guests: [
+      { id: 1, organizer: "Mario", bringingDish: "Pumpkin Pie" },
+      { id: 2, organizer: "Luigi", bringingDish: "Apple Pie" },
+      { id: 3, organizer: "Yoshi", bringingDish: "Potatoes" },
+      { id: 4, organizer: "Peach", bringingDish: "Sake" },
+    ],
+  };
 
-      const [state, setState] = useState(initialState);
-      const [guests, setGuests] = useState(initialState.guests);
-      const [editing, setEditing] = useState(false);
-      const [attending, handleAttending] = useAttending(false);
-      const userId = localStorage.getItem("user_id");
-      const {id} = useParams();
+  const [state, setState] = useState(initialState);
+  const [guests, setGuests] = useState(initialState.guests);
+  const [editing, setEditing] = useState(false);
+  const [attending, handleAttending] = useAttending(false);
 
-      console.log("eventdetails - guests:", guests); //Delete console.log
-      console.log("eventdetails - userid props:", userId); //Delete console.log
-      console.log("eventdetails - eventid:", id); //Delete console.log
+  const { id } = useParams();
 
-      const handleToggle = () => {
-        setEditing(!editing);
-      };
-      
-      useEffect(() => {
-        axiosWithAuth()
-          .get(`/organizer/${id}/guests`)
-          .then((resp) => {
-            console.log("eventdetails api resp", resp.data); //Delete console.log
-           setState(resp.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+  console.log("eventdetails - guests:", guests); //Delete console.log
+
+  console.log("eventdetails - eventid:", id); //Delete console.log
+
+  const handleToggle = () => {
+    setEditing(!editing);
+  };
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/organizer/${id}/guests`)
+      .then((resp) => {
+        console.log("eventdetails api resp", resp.data); //Delete console.log
+        setState(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
       }, []);
+  });
 
-      return (
-        <div>
-          {!editing ? (
-            <EventTitle state={state} />
-          ) : (
-            <EventTitleForm state={state} setState={setState} handleToggle={handleToggle}/>
+  return (
+    <div>
+      {!editing ? (
+        <EventTitle state={state} />
+      ) : (
+        <EventTitleForm
+          state={state}
+          setState={setState}
+          handleToggle={handleToggle}
+        />
+      )}
+      <section className="guest-list">
+        <button onClick={handleAttending} className="btn-primary">
+          Attend
+        </button>
+        <div className="attending">
+          {attending && (
+            <AddGuest
+              attending={attending}
+              handleAttending={handleAttending}
+              guests={guests}
+              setGuests={setGuests}
+            />
           )}
-          <section className="guest-list">
-            <button onClick={handleAttending} className="btn-primary">
-              Attend
-            </button>
-            <div className="attending">
-              {attending && (
-                <AddGuest attending={attending} handleAttending={handleAttending} guests={guests} setGuests={setGuests} />
-              )}
-            </div>
-          </section>
-          <section className="guest-list">
-            { guests.map((i) => (
-              <GuestDetails guests={i} key={i.id} />
-            ))}
-          </section>
-          <section className="event-edit">
-            <button onClick={handleToggle} class="btn-tertiary">
-              Edit
-            </button>
-            <button class="btn-tertiary">Delete</button>
-          </section>
         </div>
-      );
-}
+      </section>
+      <section className="guest-list">
+        {guests.map((i) => (
+          <GuestDetails guests={i} key={i.id} />
+        ))}
+      </section>
+      <section className="event-edit">
+        <button onClick={handleToggle} class="btn-tertiary">
+          Edit
+        </button>
+        <button class="btn-tertiary">Delete</button>
+      </section>
+    </div>
+  );
+};
 
 export default EventDetails;
